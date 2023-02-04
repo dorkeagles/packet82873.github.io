5f1a9b3ea7b490090426c4b07b78b5d1a06a9d94
dorkeagles <32228086+dorkeagles@users.noreply.github.com>
2023-02-04 15:27:37 +0100

    delete

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-@error_reporting(0);
-Date_default_timezone_set('GMT');$TIME = date("d-m-Y H:i:s"); $PP = getenv("REMOTE_ADDR");$J7 = simplexml_load_file("http://www.geoplugin.net/xml.gp?ip=$PP");$COUNTRY = $J7->geoplugin_countryName ;$ip = getenv("REMOTE_ADDR");$file = fopen("visit.txt","a");fwrite($file,$ip." - ".$TIME." - " . $COUNTRY ."\n") ;
-
-header("Location:./home/");
-?>
